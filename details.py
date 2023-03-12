c3c0ba3b6fac4931d09ef26f1cbed78b4a56f696
ChaudharyVivekTomarBro <125326355+ChaudharyVivekTomarBro@users.noreply.github.com>
2023-03-12 18:39:29 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "24227964"
 api_hash = "a7e531b2004c78a2091cf476fdfd50f9"
-bot_token = "6164379978:AAEjSWTwUAwcqoS8voRn4ZdDxHCccW5dwUQ"
-sudo_group = -1001814153201 
+bot_token = "6118145342:AAHnBdYXdq_MeI7w9wvPZDWnNb4uiTjb1lg"
+sudo_group = -1001721642565 
